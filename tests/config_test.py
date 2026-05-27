85ed99cd33243a58cb79d1391cdfdcda6592bad9
owen-hunter1 <70711604+owen-hunter1@users.noreply.github.com>
2026-05-27 09:39:05 -0400

    created testing and config

---
+++ tests/config_test.py
@@ -0,0 +1,14 @@
+from config import ReportConfig
+
+config_test = ReportConfig(
+    output_rows=[],
+    output_columns=[],
+    location_map={},
+    account_map={},    
+)
+
+def config_test():
+    print(config_test)
+
+
+config_test()
